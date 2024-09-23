bb35ef76e6a9cf3e593b9f591d6a90af8bff5b82
Lucas Genzelis <lucas.genzelis@gmail.com>
2024-09-23 00:15:32 -0300

    feat: exercise 5

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,6 +4,7 @@ import styled from 'styled-components/macro';
 import MaxWidthWrapper from '../MaxWidthWrapper';
 
 import VisuallyHidden from '../VisuallyHidden';
+import { QUERIES } from '../../constants';
 
 const Footer = () => {
   return (
@@ -144,6 +145,16 @@ const TopRow = styled.div`
   font-size: 0.875rem;
   border-bottom: 1px solid var(--color-gray-700);
   padding: 24px 0;
+  
+  @media ${QUERIES.tabletAndUp} {
+    flex-direction: row;
+    gap: 48px;
+    justify-content: center;
+  }
+  
+  @media ${QUERIES.laptopAndUp} {
+    justify-content: flex-end;
+  }
 `;
 
 const Social = styled.div`
@@ -170,6 +181,13 @@ const MainNavArea = styled.div`
   gap: 32px;
   padding: 32px 0 48px;
   text-align: center;
+
+  @media ${QUERIES.tabletAndUp} {
+    text-align: left;
+    display: grid;
+    gap: 32px 64px;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  }
 `;
 
 const MainNavHeading = styled.h2`
@@ -196,6 +214,10 @@ const Subfooter = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  
+  @media ${QUERIES.laptopAndUp} {
+    align-items: flex-start;
+  }
 `;
 
 const Logo = styled.a`
